9d1cd1573913b4536a7c6b1fd5cafbd4481dafeb
Liviter <lovehan27793342@gmail.com>
2024-03-16 10:10:18 +0800

    修复提示bug~

--- ShareRatio_limit/config.sh
+++ ShareRatio_limit/config.sh
@@ -88,7 +88,7 @@ while true; do
 					echo "target_share_ratio_limit=\"$target_share_ratio_limit\"" >> config.sh
 					echo "配置已保存到config.sh文件中"
 					echo "请将以下信息填入qbittorrent的相关设置中"
-					echo "复制引号内所有信息'bash $config_dir/scritpt.sh %I %L %G'填入'新增torrent时运行外部程序'"
+					echo "复制引号内所有信息'bash $config_dir/script.sh %I %L %G'填入'新增torrent时运行外部程序'"
 					exit
 				else
 					echo "连接失败，请检查设置！将重新执行脚本"
